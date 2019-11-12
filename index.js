5dea868aeb57d6d09ae55cf5b76fc2d120f375fa
sondeokhyeon <sondeokhyeon@gmail.com>
2019-11-12 13:11:28 +0900

    바벨7 버전 문제 해결

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`${PORT}포트로 서버 실행 중..`)
 })
+
+// --delay 2 --ignore 'sessions/*' --ignore 'src/asserts/*' --ignore 'src/static/*' --ignore '**.pug'",
